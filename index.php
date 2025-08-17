e1da92d947c60bc49ad82192c69c719302db3814
Anja <anjaniaina03@gmail.com>
2025-08-17 23:53:38 +0200

    Implement login page with Twig, success/error messages, and base template inheritance

--- index.php
+++ index.php
@@ -4,4 +4,19 @@ require 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('login.twig');
+$message = null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if ($username === 'user' && $password === 'password') {
+        $message = ['type' => 'success', 'text' => 'Vous êtes connecté'];
+    } else {
+        $message = ['type' => 'error', 'text' => 'Identifiants incorrects'];
+    }
+}
+
+echo $twig->render('login.twig', [
+    'message' => $message
+]);
